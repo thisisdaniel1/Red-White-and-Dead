1f24da7ce98989764afbf77a9faefaa3a0f20904
Daniel Nguyen <dknguye5@uno.edu>
2024-01-16 15:47:49 -0600

    Minor Edits To Reenable Multiplayer
    Previous Push Assumed Singleplayer, oops. Now everything should work. Also made some scripts and animator unique to each player.

--- Assets/Scenes/TestScene/Scripts/Player/PlayerSetup.cs
+++ Assets/Scenes/TestScene/Scripts/Player/PlayerSetup.cs
@@ -4,10 +4,19 @@ public class PlayerSetup : MonoBehaviour
 {
     public PlayerController playerController;
 
+    public CharacterRigController characterRigController;
+
+    public Actions actions;
+
+    public Animator animator;
+
     public GameObject cam;
 
     public void IsLocalPlayer(){
         playerController.enabled = true;
+        characterRigController.enabled = true;
+        actions.enabled = true;
+        animator.enabled = true;
         cam.SetActive(true);
     }
 }
