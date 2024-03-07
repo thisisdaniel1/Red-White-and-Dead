68a9c2375183ffc00ae43ee2dbd7e1c557e0dae8
Daniel Nguyen <dknguye5@uno.edu>
2024-03-07 16:09:53 -0600

    Bidirectional sprites in 3d world
    https://www.youtube.com/watch?v=UcYfEfJW_mk&t=378s

---
+++ Assets/Scenes/TestScene/Scripts/Billboarding.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Billboarding : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Camera is always updated first
+    void LateUpdate()
+    {
+        if (Camera.main != null){
+            GameObject nearestPlayer = FindNearestPlayer();
+            
+            if (nearestPlayer != null){
+                Vector3 directionToPlayer = nearestPlayer.transform.position - transform.position;
+                // This ensures that the zombie only rotates around the y-axis
+                directionToPlayer.y = 0; 
+
+                // Smoothly rotate towards the player
+                Quaternion lookRotation = Quaternion.LookRotation(directionToPlayer);
+                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+            }
+        }
+    }
+
+    GameObject FindNearestPlayer()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        GameObject nearestPlayer = null;
+        float minDistance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach(GameObject player in players)
+        {
+            float distance = Vector3.Distance(player.transform.position, position);
+            if(distance < minDistance)
+            {
+                nearestPlayer = player;
+                minDistance = distance;
+            }
+        }
+        return nearestPlayer;
+    }
+}
---
+++ Assets/Scenes/TestScene/Scripts/SpriteDirectionalController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class SpriteDirectionalController : MonoBehaviour
+{
+    public float backAngle = 65f;
+    public float sideAngle = 155f;
+    public Transform mainTransform;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    // Camera is updated first
+    void LateUpdate()
+    {
+        Vector3 camForwardVector = new Vector3(Camera.main.transform.forward.x, 0f, Camera.main.transform.forward.z);
+        //Debug.DrawRay(Camera.main.transform.position, camForwardVector + 5f, Color.magenta);
+
+        float signedAngle = Vector3.SignedAngle(mainTransform.forward, camForwardVector, Vector3.up);
+
+        Vector2 animationDirection = new Vector2(0f, -1f);
+
+        float angle = Mathf.Abs(signedAngle);
+
+        if (angle < backAngle){
+            // back animation
+            animationDirection = new Vector2(0f, -1f);
+        }
+        else if (angle < sideAngle){
+            // side animation
+            animationDirection = new Vector2(1f, 0f);
+        }
+        else{
+            // front animation
+            animationDirection = new Vector2(0f, 1f);
+        }
+
+        animator.SetFloat("moveX", animationDirection.x);
+        animator.SetFloat("moveY", animationDirection.y);
+    }
+}
